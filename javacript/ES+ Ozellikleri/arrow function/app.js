9123c212d7703844c492cef9d6da8af87f6ffd9d
EMIN ELEKBERLI <elekberli1996@gmail.com>
2022-04-23 00:08:06 +0400

    yeni

---
+++ javacript/ES+ Ozellikleri/arrow function/app.js
@@ -0,0 +1,14 @@
+const mehaba= () =>{
+
+    console.log("mehfdfaba");
+}
+
+
+mehaba();
+const mehabaa= input =>{// bir prarametre girsek balaca ()moterzeye etiyac yoxdu
+
+    console.log("mehfdjjjjjjfaba",input);
+}
+mehabaa("ssaqqqqqqqqq")
+
+const mehabaaaa= input => console.log("mehfdjjjjjjfaba",input);  // bir islem yaparsa moterzeye{} rutern -e etiyac yoxdu
